d0efa48c69b0c5eeb0ee13d6e25e0aa8b447ea3f
ganguoguo <546712163@qq.com>
2020-02-07 15:21:16 +0800

    1

--- test1/src/test1/test2.java
+++ test1/src/test1/test2.java
@@ -1,3 +1,10 @@
-public static void main(String[] args){
-  System.out.println("12313");
+package test1;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		System.out.println("123132");
+
+	}
+
 }
